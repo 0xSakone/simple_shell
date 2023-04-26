bae04a2a7def6d9acbd67053873f8ee1c5efc805
0xSakone <abdramanesakone4@gmail.com>
2023-04-26 23:26:12 +0200

    ✨ update output 1.35

--- head.h
+++ head.h
@@ -1,6 +1,8 @@
 #ifndef HEAD_HEADER
 #define HEAD_HEADER
 
+#include <signal.h>
+
 char *_strncpy(char *dest, char *src, int n);
 
 #endif
--- main.c
+++ main.c
@@ -6,6 +6,18 @@
 #include "main.h"
 
 /**
+ * prompt - show prompt
+ * @signal: The signal.
+ */
+void prompt(int signal)
+{
+	char *new_prompt = "\n$ ";
+
+	(void)signal;
+	write(STDIN_FILENO, new_prompt, 3);
+}
+
+/**
  * getCommand - function that get the command from string
  * @output: the cmd return
  * @input: the given string
@@ -30,6 +42,50 @@ int getCommand(char *output, char *input)
 }
 
 /**
+ * run - execute program with args
+ * @cmd: program to run
+ * @pargs: program argument
+ * @envp: program environment
+ * @ret: return value pointer
+ * @pprogram: parent program name
+ * Return: 0 as success
+ */
+int run(char *cmd, char *pargs, char **envp, int *ret, char *pprogram)
+{
+	int rvalue = 0, status;
+	char *args[] = {NULL, NULL};
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1)
+		perror(pprogram);
+	else if (pid == 0)
+	{
+		args[0] = cmd;
+		getCommand(cmd, pargs);
+		rvalue = execve(cmd, args, envp);
+		if (rvalue < 1)
+		{
+			perror(pprogram);
+			*ret = rvalue;
+			return (-1);
+		}
+		fflush(stdin);
+		signal(SIGINT, prompt);
+		return (0);
+	}
+	else
+	{
+		if (wait(&status) == -1)
+		{
+			perror(pprogram);
+			return (-1);
+		}
+	}
+	return (0);
+}
+
+/**
  * main - entry function
  * @argc: number of argument passed to the programm
  * @argv: arguments passed
@@ -42,39 +98,24 @@ int main(__attribute__((unused)) int argc,
 {
 	char *user_input, *cmd;
 	size_t input_size = 128;
-	char *args[] = {NULL, NULL};
-	pid_t pid;
-	int status;
+	int ret;
 
 	cmd = (char *)malloc(input_size * sizeof(char));
 	user_input = (char *)malloc(input_size * sizeof(char));
 	while (1)
 	{
-		_printf("#cisfun$ ");
-		if (getline(&user_input, &input_size, stdin) > 0)
+		write(STDOUT_FILENO, "$ ", 2);
+		if (getline(&user_input, &input_size, stdin) != -1)
 		{
-			pid = fork();
-			if (pid == -1)
-				perror("fork");
-			else if (pid == 0)
+			cmd = (char *)malloc(128 * sizeof(char));
+			if (run(cmd, user_input, envp, &ret, argv[0]) == -1)
 			{
-				cmd = (char *)malloc(128 * sizeof(char));
-				args[0] = cmd;
-				getCommand(cmd, user_input);
-				if (execve(cmd, args, envp) < 1)
-					perror(argv[0]);
-				free(cmd);
-			}
-			else
-			{
-				if (wait(&status) == -1)
-					perror("wait");
+				exit(ret);
 			}
 		}
 		else
 		{
 			free(user_input);
-			fflush(STDIN_FILENO);
 			break;
 		}
 	}
